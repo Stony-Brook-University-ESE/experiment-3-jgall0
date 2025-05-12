e2a5a2b51f3303c587a8d95706d3da05633e24df
jgall0 <jgallo.ny23@gmail.com>
2025-05-12 12:55:34 -0400

    end of experiment

--- Build/Experiment3.c
+++ Build/Experiment3.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-
+#define n 1000
 int main() {
-    int n = 1000;
-    int matrix[n][n];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    long int matrix[n][n];
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             matrix[i][j] = i * n + j + 1;
         }
     }
-
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = n - 1; j >= 0; j--) {
-            printf("%4d ", matrix[i][j]);
+    for (i = n - 1; i >= 0; i--) {
+        for (j = n - 1; j >= 0; j--) {
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
